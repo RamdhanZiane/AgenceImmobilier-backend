9cef90f9590052741e563aeb844fc85ad109ec22
Ramdhan <issakalem@gmail.com>
2023-01-18 18:15:23 +0100

    start testing

--- ImmobilApp/agence_api/tests.py
+++ ImmobilApp/agence_api/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Annonce
 
 # Create your tests here.
+
+class AnnonceTestCase(TestCase):
+    def setUp(self) -> None:
+        client = Client()
+        Annonce.objects.create(annonceur=1, titre='titre annonce', categorie='V', type='T', description='no desc', prix=44, surface=4)
+
+    def has_desc(self):
+        # Annonce.objects.get(annonceur=1)
+        # self.assertEqual()
+        res = self.client.get('/agence-api/annonces')
+        self.assertEqual(res.status_code, 200)
